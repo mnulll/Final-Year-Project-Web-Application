f87a74270e016a25a81d142c5be2e9b734b17a5f
Luqmanul Hakim <78916240+mnulll@users.noreply.github.com>
2023-01-10 00:15:14 +0800

    major update on app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from utils import data_preprocessing,predict
 import numpy as np
 import webbrowser
+import csv
 
 menu = ["About","Predict Your Emotion!","Feedback"]
 choice = st.sidebar.selectbox("Menu",menu)
@@ -14,8 +15,30 @@ if choice =="About":
         after consuming a variety of drinks, as well as the use of neural networks to predict the emotional state of users based on their physiological data </div>''', unsafe_allow_html=True )
 
 elif choice=="Predict Your Emotion!":
-    st.subheader("Dataset")
-    uploaded_files=st.file_uploader("Upload CSV",type=['csv'],accept_multiple_files=True)
+    st.markdown("\n\n\n\n ## Try Out With Our Sample Data ! \n\n")
+
+    if st.button('Use Sample Data'):
+        
+
+        HR = open('HR.csv',"r")
+        EDA=open("EDA.csv","r")
+        input=data_preprocessing(HR,EDA)
+        
+        class_pred=predict(input)
+
+        if class_pred==0:
+            st.success("Class 0 - Low Arousal Negative Valence")
+        elif class_pred==1:
+            st.success("Class 1 - Low Arousal Positive Valence")
+        elif class_pred==2:
+            st.success("Class 2 - High Arousal Negative Valence")
+        else:
+            st.success("Class 3 - High Arousal Positive Valence")
+    
+    st.empty()
+    st.markdown("\n\n\n\n ## Use Your Own Data ! \n\n")
+
+    uploaded_files=st.file_uploader("*** Rename your heart rate file as \"hr.csv\" and your eda or gsr file as \"eda.csv\" *** ",type=['csv'],accept_multiple_files=True)
     if st.button("Process"):
         input=np.array([])
         if uploaded_files[0].name.lower()=="hr.csv" and uploaded_files[1].name.lower()=="eda.csv"  :
@@ -34,17 +57,14 @@ elif choice=="Predict Your Emotion!":
         else:
             st.success("Class 3 - High Arousal Positive Valence")
     
-    url = 'https://docs.google.com/forms/d/e/1FAIpQLScR7C1WY1GFew2Rqx-ykUNjbZZUyuxFxxyOw86aE0LOiUpZ0A/viewform?usp=sf_link'
-
-    if st.button('Use Our Sample Data!'):
-        
         # if hrv_file is not None and hrv_file:
         #     input_model=data_preprocessing(hrv_file)
         #     result=predict(input_model)
         #     st.write(result)
 
+
 elif choice=="Feedback":
-    st.subheader("Submit Your Feedback!")
+    st.subheader("Submit Your Feedback !")
     url = 'https://docs.google.com/forms/d/e/1FAIpQLScR7C1WY1GFew2Rqx-ykUNjbZZUyuxFxxyOw86aE0LOiUpZ0A/viewform?usp=sf_link'
 
     if st.button('Submit'):
