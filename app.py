cd8b88c93fb38553b3594641702378254bc10d3d
Luqmanul Hakim <78916240+mnulll@users.noreply.github.com>
2023-01-10 18:36:36 +0800

    Update on web application

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 from bokeh.models.widgets import Div
 import webbrowser
 import csv
+from PIL import Image
 
 menu = ["About","Predict Your Emotion!","Feedback"]
 choice = st.sidebar.selectbox("Menu",menu)
@@ -14,6 +15,52 @@ if choice =="About":
     st.markdown(
         '''<div style="text-align: justify;">This study investigated the use of physiological data to assess the emotional state of users 
         after consuming a variety of drinks, as well as the use of neural networks to predict the emotional state of users based on their physiological data </div>''', unsafe_allow_html=True )
+
+    for i in range(3):
+        st.write(" ")
+
+    st.subheader("Guidance On Using Web Application")
+
+    st.write("1. Click on the Predict Your Emotion")
+    guidance1=Image.open("Guidance Images/1.png")
+    st.image(guidance1)
+
+    st.write("")
+    st.write("2. Click on Use Sample Data if you want to demonstrate the prediction using the sample data ")
+    guidance2=Image.open("Guidance Images/2.png")
+    st.image(guidance2)
+
+    st.write("")
+    st.write("3. The web application will show the data analysis and the class prediction")
+    guidance3=Image.open("Guidance Images/3.png")
+    st.image(guidance3)
+
+    st.write("")
+    st.write("4. If you want to use your data for prediction , upload the files and click Process")
+    guidance4=Image.open("Guidance Images/4.png")
+    st.image(guidance4)
+
+    st.write("")
+    st.write("5. The web application will show the data analysis and the class prediction")
+    st.image(guidance3)
+
+    st.write("")
+    st.write("6. Click on Feedback to give your feedback about the web application")
+    guidance5=Image.open("Guidance Images/5.png")
+    st.image(guidance5)
+
+    st.write("")
+    st.write("7. Click on Submit Button")
+    guidance6=Image.open("Guidance Images/6.png")
+    st.image(guidance6)
+
+    st.write("")
+    st.write("8.The page of Google Form will be prompted. You are required to answer all of the questions")
+    guidance7=Image.open("Guidance Images/7.png")
+    st.image(guidance7)
+
+
+
 
 elif choice=="Predict Your Emotion!":
     st.markdown("\n\n\n\n ## Try Out With Our Sample Data ! \n\n")
